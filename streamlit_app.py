ecf0dcf0c2d4eba100fcadd522abca9a9306c390
Muh. Rivaldi Prabowo <99151517+rivaldiprabowo@users.noreply.github.com>
2022-12-16 13:34:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-steramlit.header('Breakfast Menu')
+#steramlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spincah & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
