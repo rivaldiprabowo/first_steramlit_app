3391218412f5635dfa1fe6eee204f883af15b3f1
Muh. Rivaldi Prabowo <99151517+rivaldiprabowo@users.noreply.github.com>
2022-12-18 18:28:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #Snowflake Challenge
-fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
